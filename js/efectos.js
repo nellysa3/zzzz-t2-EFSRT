26d1bc113fb16efcf3a36f9216a63f6ba60c7438
Nelly Suarez Saavedra <nellysa3@localhost>
2024-10-20 01:47:39 -0500

    agregar js

--- js/efectos.js
+++ js/efectos.js
@@ -2,3 +2,8 @@ function toggleMenu() {
     var nav = document.getElementById('navbar');
     nav.classList.toggle('active');
 }
+
+function registrarCita(){
+    alert("Registrado exitosamente")
+}
+
